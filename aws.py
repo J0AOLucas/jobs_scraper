9b9f84d831279213878ba55e6c099247500f2d91
Joao Lucas <lucasrpg.silva@gmail.com>
2025-02-08 12:35:27 -0800

    feature/sending-all-jobs-to-spreadsheet

--- aws.py
+++ aws.py
@@ -7,59 +7,56 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
-import pdb
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
+def get_aws_jobs():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
 
-url = f"https://www.amazon.jobs/content/en/teams/amazon-web-services/sales?role-type%5B%5D=0&category%5B%5D=Sales%2C+Advertising%2C+%26+Account+Management&employment-type%5B%5D=Full+time&country%5B%5D=US"
+    url = f"https://www.amazon.jobs/content/en/teams/amazon-web-services/sales?role-type%5B%5D=0&category%5B%5D=Sales%2C+Advertising%2C+%26+Account+Management&employment-type%5B%5D=Full+time&country%5B%5D=US"
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get(url)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get(url)
 
-wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10)
 
-page_number = 1
-jobs = []
-time.sleep(5)
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-pages_count = int(soup.find_all('li', 'css-lsly4i ehuj7it1')[-1].text)
+    page_number = 1
+    jobs = []
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    pages_count = int(soup.find_all('li', 'css-lsly4i ehuj7it1')[-1].text)
 
-while True:
-    for x in range(pages_count):
+    while True:
+        for x in range(pages_count):
+            
+            time.sleep(3)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            job_elements = soup.find_all('div', class_='job-card-module_root__QYXVA')
+
+            if job_elements:
+                for job_element in job_elements:
+                    job_title = job_element.find('h3').text
+                    job_link = job_element.find('a')['href']
+                    job_link = f"https://www.amazon.jobs/en{job_link}/{job_title}"
+
+                    jobs.append({
+                        'company': 'Amazon Web Services (AWS)',
+                        'title': job_title,
+                        'link': job_link
+                    })
+            else:
+                break
+            
+            #next_button = soup.find('button', 'e4s17lp0 css-g28onq')
+            next_button = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='search']/div/div[2]/div[2]/nav/button[2]/div")))
+            driver.execute_script("arguments[0].click();", next_button)
+            print(f"Going to next page...")
         
-        time.sleep(3)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        job_elements = soup.find_all('div', class_='job-card-module_root__QYXVA')
+        break
 
-        if job_elements:
-            for job_element in job_elements:
-                job_title = job_element.find('h3').text
-                job_link = job_element.find('a')['href']
-                job_link = f"https://www.amazon.jobs/en{job_link}/{job_title}"
-
-                jobs.append({
-                    'company': 'Amazon Web Services (AWS)',
-                    'title': job_title,
-                    'link': job_link
-                })
-        else:
-            break
-        
-        #next_button = soup.find('button', 'e4s17lp0 css-g28onq')
-        next_button = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='search']/div/div[2]/div[2]/nav/button[2]/div")))
-        driver.execute_script("arguments[0].click();", next_button)
-        print(f"Going to next page...")
-    
-    break
-
-driver.quit()
-for job in jobs:
-    print(job)
-
-print('Finished!')
-print(len(jobs))
+    driver.quit()
+    print('AWS Finished!')
+    return jobs
         
--- microsoft.py
+++ microsoft.py
@@ -14,75 +14,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-from firebase import send_data
-import pdb
-import platform
 
+def get_microsoft_jobs():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
 
-# Configurações do navegador
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
+    url = f"https://jobs.careers.microsoft.com/global/en/search?q=technology%20sales&lc=United%20States&d=Technology%20Specialists&rt=Individual%20Contributor&l=en_us&pg=1&pgSz=20&o=Relevance&flt=true"
 
-url = f"https://jobs.careers.microsoft.com/global/en/search?q=technology%20sales&lc=United%20States&d=Technology%20Specialists&rt=Individual%20Contributor&l=en_us&pg=1&pgSz=20&o=Relevance&flt=true"
+    # Usando o webdriver-manager para gerenciar o chromedriver
 
-# Usando o webdriver-manager para gerenciar o chromedriver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get(url)
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get(url)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "ms-List-cell"))
+    )
 
-WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "ms-List-cell"))
-)
+    driver.implicitly_wait(10)
+    html = driver.page_source
+    driver.quit()
 
-# Espera para garantir que a página carregue completamente
-driver.implicitly_wait(10)
+    soup = BeautifulSoup(html, 'html.parser')
+    job_elements = soup.find_all('div', class_='ms-List-cell')
+    jobs = []
 
-# Pegando o HTML renderizado
-html = driver.page_source
+    for job_element in job_elements:
+        job_title = job_element.find('h2', class_='MZGzlrn8gfgSs8TZHhv2')
+        job_stack = job_element.find('div', class_='ms-Stack')
+        job_id = job_stack.get('aria-label').split()[-1] if job_stack else 'N/A'
+        job_link = (f"https://jobs.careers.microsoft.com/global/en/job/{job_id}/{job_title.text}").replace(' ', '%20')
 
-# Fechando o navegador
-driver.quit()
+        #date_posted_element = job_element.find('span', string=lambda text: 'days ago' in text or 'month ago' in text)
+        #description_element = job_element.find('div', class_='ms-Stack')
+        #location_element = job_element.find('span', string=lambda text: 'Locations' in text)
 
-# Usando BeautifulSoup para analisar o HTML
-soup = BeautifulSoup(html, 'html.parser')
-job_elements = soup.find_all('div', class_='ms-List-cell')
-jobs = []
+        title = job_title.text if job_title else 'N/A'
+        #link = link_element['aria-label'] if link_element else 'N/A'
+        #description = description_element.text if description_element else 'N/A'
+        #location = location_element.text if location_element else 'N/A'
+        #date_posted = date_posted_element.text if date_posted_element else 'N/A'
 
-for job_element in job_elements:
-    job_title = job_element.find('h2', class_='MZGzlrn8gfgSs8TZHhv2')
-    job_stack = job_element.find('div', class_='ms-Stack')
-    job_id = job_stack.get('aria-label').split()[-1] if job_stack else 'N/A'
-    job_link = (f"https://jobs.careers.microsoft.com/global/en/job/{job_id}/{job_title.text}").replace(' ', '%20')
+        jobs.append({
+            'company': 'Microsoft',
+            'title': title,
+            'link': job_link,
+        })
 
-    #date_posted_element = job_element.find('span', string=lambda text: 'days ago' in text or 'month ago' in text)
-    #description_element = job_element.find('div', class_='ms-Stack')
-    #location_element = job_element.find('span', string=lambda text: 'Locations' in text)
-
-    title = job_title.text if job_title else 'N/A'
-    #link = link_element['aria-label'] if link_element else 'N/A'
-    #description = description_element.text if description_element else 'N/A'
-    #location = location_element.text if location_element else 'N/A'
-    #date_posted = date_posted_element.text if date_posted_element else 'N/A'
-
-    jobs.append({
-        'Company': 'Microsoft',
-        'title': title,
-        'link': job_link,
-    })
-
-for job in jobs:
-    #print(f"Description: {job['description']}")
-    #print(f"Location: {job['location']}")
-    #print(f"Date Posted: {job['date_posted']}")
-    print('Company: Microsoft')
-    print(f"Title: {job['title']}")
-    print(f"Link: {job['link']}")
-    print('-' * 40)
-
-for job in jobs:
-    send_data(job)
-
-print('Finished!')
+    print('Microsoft Finished!')
+    return jobs
